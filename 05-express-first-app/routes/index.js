19cc62dcbf932513e312bfb127daea970535f56b
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-03-17 17:17:40 +0100

    Fixed wrong commit

---
+++ 05-express-first-app/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Cool, huh!', condition: false });
+});
+
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/users/detail', function(req, res, next) {
+  res.send('detail');
+});
+
+
+module.exports = router;
