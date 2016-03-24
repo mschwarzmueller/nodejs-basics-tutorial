24d21bedc2aebf02a2a71dc78e8f471e74e7d519
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-03-24 15:25:40 +0100

    Added validation and mongodb sections

---
+++ 08-validation-sessions/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+  req.session.errors = null;
+});
+
+router.post('/submit', function(req, res, next) {
+  req.check('email', 'Invalid email address').isEmail();
+  req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
+
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  } else {
+    req.session.success = true;
+  }
+  res.redirect('/');
+});
+
+module.exports = router;
---
+++ 09-mongodb/routes/index.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/get-data', function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('user-data').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('index', {items: resultArray});
+    });
+  });
+});
+
+router.post('/insert', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/');
+});
+
+router.post('/update', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item updated');
+      db.close();
+    });
+  });
+});
+
+router.post('/delete', function(req, res, next) {
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').deleteOne({"_id": objectId(id)}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item deleted');
+      db.close();
+    });
+  });
+});
+
+module.exports = router;
